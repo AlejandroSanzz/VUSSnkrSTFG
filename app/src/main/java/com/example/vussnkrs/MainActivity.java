ec42e36c72b0f07123a26fb4cd82640f5e486c6f
alex <alexsanchezherna@gmail.com>
2023-05-17 23:36:39 +0200

    Inicio sesión y registrarse al inicio

--- app/src/main/java/com/example/vussnkrs/MainActivity.java
+++ app/src/main/java/com/example/vussnkrs/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.vussnkrs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -8,39 +9,89 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
-    SharedPreferences pref;
-    EditText text_nombre, text_talla;
-    String miNombre, miTalla;
-    TextView text_bienvenida;
+
+    FirebaseAuth mAuth;
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //preft = getSharedPreferences(getString(R.string.talla), Context.MODE_PRIVATE);
-       /* text_talla = findViewById(R.id.text_talla);
-        SharedPreferences pref = getSharedPreferences(getString(R.string.pref_talla), Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
-        editor.putString("talla", text_talla.getText().toString());
-        editor.apply();
-        txt_saludo.setText("Bienvenido a VUSSnkrS " + nombre + " tu talla es la " + talla);
-        */pref = getPreferences(Context.MODE_PRIVATE);
+        mAuth = FirebaseAuth.getInstance();
+
+        Button btInicioSesion = findViewById(R.id.btn_iniciar_sesion);
+        Button btRegistro = findViewById(R.id.btn_registrarse);
+        EditText emailInicioSesion = findViewById(R.id.txt_email_inicio_sesion);
+        EditText passwordInicioSesion = findViewById(R.id.txt_contrasena_inicio_sesion);
+
+        btInicioSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailUSer = emailInicioSesion.getText().toString().trim();
+                String passUSer = passwordInicioSesion.getText().toString().trim();
+
+                if (emailUSer == null && passUSer == null) {
+                    Toast.makeText(MainActivity.this, "Ingresar los datos", Toast.LENGTH_SHORT).show();
+                } else {
+                    loginUser(emailUSer, passUSer);
+                }
+
+            }
+        });
+
+        btRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                startActivity(new Intent(MainActivity.this, RegistroActivity.class));
+                Toast.makeText(MainActivity.this, "Registrarse", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        //pref = getPreferences(Context.MODE_PRIVATE);
        // text_nombre = findViewById(R.id.txt_nombre);
-        miNombre = pref.getString("miNombre","stranger");
+        //miNombre = pref.getString("miNombre","stranger");
         /*text_talla = findViewById(R.id.text_talla);
         miTalla = pref.getString("miTalla","0");
         *///nVeces = Integer.parseInt(prefv.getString("nVeces","0"));
         //text_veces = findViewById(R.id.text_veces);
-        text_bienvenida = findViewById(R.id.txt_bienvenida);
-        text_bienvenida.setText("Bienvenido de nuev@ " + miNombre + "!!");
+        //text_bienvenida = findViewById(R.id.txt_bienvenida);
+        //text_bienvenida.setText("Bienvenido de nuev@ " + miNombre + "!!");
     }
 
-    public void irTienda (View view) {
+    private void loginUser(String emailUSer, String passUSer) {
+        mAuth.signInWithEmailAndPassword(emailUSer, passUSer).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    finish();
+                    startActivity(new Intent(MainActivity.this, PaginaActivity.class));
+                    Toast.makeText(MainActivity.this, "Bienvenido", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /*public void irTienda (View view) {
         Intent i = new Intent(this, PaginaActivity.class);
         startActivity(i);
         /*
@@ -58,12 +109,12 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
         } else {
             text_nombre.setError("El campo nombre es obligatorio");
-        }*/
+        }
 
-    }
+    } */
 
-    public void admin (View view) {
+    /*public void admin (View view) {
         Intent i = new Intent(this, AdminActivity.class);
         startActivity(i);
-    }
+    }*/
 }
